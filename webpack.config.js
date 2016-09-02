73ee2358fb7700d035c6a19f7c7c10a657bbd760
Ilya Ivanov <static.ila@gmail.com>
2016-09-02 09:30:12 +0300

    Initial webpack configuration
    Supports: ES2015, multiple entry points, minification, NODE_ENV
    parameters, source maps

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+'use strict';
+const webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isDev = NODE_ENV == 'development';
+
+module.exports = {
+    context: __dirname + '/src',
+
+    entry: {
+        home: './home',
+        about: './about'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js',
+        library: '[name]'
+    },
+
+    watch: isDev,
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: isDev ? 'inline-source-map' : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV),
+            LANG: JSON.stringify('ru')
+        }),
+    ],
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+
+    //optimizations: better build
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js']
+    },
+
+    resolveLoader: {
+        modulesDirectories: ['node_modules'],
+        moduleTemplates: ['*-loader', '*'],
+        extensions: ['', '.js']
+    },
+};
+
+if (NODE_ENV == 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                // don't show unreachable variables etc
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    );
+}
